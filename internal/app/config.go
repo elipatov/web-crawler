fa2d3c7d9a40e8e5144d87a9074ce930e343d4a6
Evgeni Lipatov <evgeni.lipatov@winfinity.live>
2026-07-14 15:14:57 +0300

    fix: issues

--- internal/app/config.go
+++ internal/app/config.go
@@ -6,18 +6,17 @@ type Config struct {
 	NATS           natsConfig
 	HTTPAddress    string `env:"HTTP_ADDRESS" envDefault:":8080"`
 	Elasticsearch  elasticsearchConfig
-	Concurrency    int           `env:"CONCURRENCY" envDefault:"1"`
+	Concurrency    int           `env:"CONCURRENCY" envDefault:"2"`
 	ReprocessDelay time.Duration `env:"REPROCESS_DELAY" envDefault:"60s"`
 	TTL            time.Duration `env:"TTL" envDefault:"720h"`
 }
 
 type natsConfig struct {
-	URL         string `env:"NATS_URL" envDefault:"nats://nats-1:4222"`
-	Stream      string `env:"NATS_STREAM" envDefault:"webcrawler"`
-	Subject     string `env:"NATS_SUBJECT" envDefault:"resources"`
-	Bucket      string `env:"NATS_BUCKET" envDefault:"webcrawler"`
-	ConsumerID  string `env:"NATS_CONSUMER_ID" envDefault:"webcrawler"`
-	Concurrency int    `env:"NATS_CONCURRENCY" envDefault:"2"`
+	URL        string `env:"NATS_URL" envDefault:"nats://nats-1:4222"`
+	Stream     string `env:"NATS_STREAM" envDefault:"webcrawler"`
+	Subject    string `env:"NATS_SUBJECT" envDefault:"resources"`
+	Bucket     string `env:"NATS_BUCKET" envDefault:"webcrawler"`
+	ConsumerID string `env:"NATS_CONSUMER_ID" envDefault:"webcrawler"`
 }
 
 type elasticsearchConfig struct {
